df0edec0c65acebc8a77cbabd50de7a6f81caf66
MrPibol <mr.jp.torres@gmail.com>
2017-11-12 08:35:59 +0000

    Lesson 35: Making a dropdown

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,16 @@
 class Comment < ApplicationRecord
     belongs_to :user
     belongs_to :place
+    
+    RATINGS = {
+        'ONE STAR': '1_STAR',    
+        'TWO STARS': '2_STARS',   
+        'THREE STARS': '3_STARS',   
+        'FOUR STARS': '4_STARS',   
+        'FIVE STARS': '5_STARS'
+    }
+
+def humanized_rating
+    RATINGS.invert[self.rating]     
+end
 end
